c0e1e18de69bc04c76169f684d40c26877686091
Oscar <dptocalidadicg@gmail.com>
2022-04-06 13:04:20 +0200

    Commit inicial

---
+++ src/AbrePortalRestPackage/VerificarCookies.java
@@ -0,0 +1,29 @@
+package AbrePortalRestPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class VerificarCookies extends TestBase {
+	
+	 @Test (description="Verifica si sale o no la ventana de aceptación de cookies", priority=1)
+	  public void verificarCookies() {
+		  WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(2));
+		  w.until(ExpectedConditions.presenceOfElementLocated (By.className("cookies-info-content")));
+		  Reporter.log("Hemos encontrado la classe cookies-info-content");
+		  
+	  }
+	 
+	 //Solo se ejecuta si el test de verificar cookies es correcto.
+	 @Test (description="Acepta cookies",  dependsOnMethods="verificarCookies", priority=2)
+	  public void aceptaCookies() {
+		  WebElement aceptarCookies = driver.findElement(By.className("btn-confirm"));
+		  aceptarCookies.click();
+		  Reporter.log("Hemos aceptado las cookies");  
+	  }
+}
---
+++ src/AbrePortalRestPackage/VerificarFichaRestaurante.java
@@ -0,0 +1,24 @@
+package AbrePortalRestPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VerificarFichaRestaurante extends TestBase {
+  
+	@Test(description="Este test abre la URL proporcionada para abrir PortalRest", priority=1)
+	  @Parameters({"expectedTitle", "classExpected" })
+	  public void verificaFichaRestaurante(String expectedTitle, String classExpected) {
+	      WebDriverWait w = new WebDriverWait(TestBase.driver,Duration.ofSeconds(10));
+	      w.until(ExpectedConditions.presenceOfElementLocated (By.className(classExpected)));
+	      WebElement tituloRestaurante = driver.findElement(By.className(classExpected));
+	      Reporter.log("Hemos encontrado la classe " +classExpected+" con valor: " + tituloRestaurante.getAttribute("innerHTML")); 
+	  }
+
+}
